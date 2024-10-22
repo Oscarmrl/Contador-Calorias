19999fbfccae69c35fddf9e02354545fc7922be8
Oscarmrl <oscaralexandermurillo500@gmail.com>
2024-10-22 16:21:56 -0600

    Reinicio completo Actividades

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useReducer } from "react";
+import { useReducer, useEffect, useMemo } from "react";
 import { initialState, activityReducer } from "./reducers/activity-reducer";
 import Form from "./components/Form.";
 import ActivityList from "./components/ActivityList";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
+
+  const RestartApp = useMemo(
+    () => state.activities.length > 0,
+    [state.activities]
+  );
 
   return (
     <>
@@ -13,6 +22,13 @@ function App() {
           <h1 className=" text-center font-bold text-white uppercase">
             Contador de Calorias
           </h1>
+          <button
+            className=" bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white text-sm rounded-lg disabled:opacity-50"
+            disabled={!RestartApp}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
 
